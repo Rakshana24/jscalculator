67194f35cce078f72fb429d7c77ebf7530eef975
Rakshana24 <rakshanat.cb24@bitsathy.ac.in>
2024-11-07 11:08:58 +0530

    Add files via upload

---
+++ sor.js
@@ -0,0 +1,19 @@
+function working(value) {
+    const disp = document.getElementById("display");
+    disp.value += value;
+    console.log(disp.value);
+}
+
+function clearDisplay() { 
+    const disp = document.getElementById("display");
+    disp.value = ""; 
+}
+
+function calculateResult() {
+    const disp = document.getElementById("display"); 
+        disp.value = eval(disp.value);
+        console.log(disp.value);
+    } catch (error) {
+        disp.value = "error"; 
+    }
+}
